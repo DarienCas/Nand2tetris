1b4a77d74ff1e63c42ed0a607ce300bd9b65afbd
DarienCas <144241018+DarienCas@users.noreply.github.com>
2023-09-07 14:34:15 -0500

    Update Readme.ml

--- Lab 01/Readme.ml
+++ Lab 01/Readme.ml
@@ -1,1 +1,55 @@
 
+Este informe tiene como objetivo proporcionar una visión general de la creación de varios chips de puertas lógicas y dispositivos de multiplexión/demultiplexión. Estos chips son componentes fundamentales en el diseño y la construcción de circuitos digitales y sistemas computacionales. Se han desarrollado los siguientes chips:
+
+1. Not (Not Gate)
+
+Función: Realiza la operación lógica NOT, invirtiendo la señal de entrada.
+2. And (And Gate)
+
+Función: Realiza la operación lógica AND, produciendo una salida 1 solo si ambas entradas son 1.
+3. Or (Or Gate)
+
+Función: Realiza la operación lógica OR, produciendo una salida 1 si al menos una de las entradas es 1.
+4. Xor (Exclusive-Or Gate)
+
+Función: Realiza la operación lógica XOR, produciendo una salida 1 si las entradas son diferentes.
+5. Mux (Multiplexor)
+
+Función: Permite seleccionar entre dos fuentes de datos utilizando una señal de control.
+6. DMux (Demultiplexor)
+
+Función: Distribuye una señal de entrada en una de dos salidas basándose en una señal de control.
+7. Not16 (16-bit Not)
+
+Función: Aplica la operación NOT a cada bit de una entrada de 16 bits.
+8. And16 (16-bit And)
+
+Función: Realiza la operación AND a nivel de bit en dos entradas de 16 bits.
+9. Or16 (16-bit Or)
+
+Función: Realiza la operación OR a nivel de bit en dos entradas de 16 bits.
+10. Mux16 (16-bit Multiplexor)
+- Función: Un multiplexor de 16 bits que selecciona entre dos entradas de 16 bits basándose en una señal de control.
+
+11. Or8Way (8-way Or)
+- Función: Realiza la operación OR en ocho entradas y produce una única salida.
+
+12. Mux4Way16 (4-way 16-bit Multiplexor)
+- Función: Un multiplexor de 16 bits que selecciona entre cuatro entradas de 16 bits basándose en dos señales de control.
+
+13. Mux8Way16 (8-way 16-bit Multiplexor)
+- Función: Un multiplexor de 16 bits que selecciona entre ocho entradas de 16 bits basándose en tres señales de control.
+
+14. DMux4Way (4-way Demultiplexor)
+- Función: Distribuye una señal de entrada en una de cuatro salidas basándose en dos señales de control.
+
+15. DMux8Way (8-way Demultiplexor)
+- Función: Distribuye una señal de entrada en una de ocho salidas basándose en tres señales de control.
+
+Conclusión
+Estos chips y dispositivos representan la base de la electrónica digital y son esenciales en la construcción de sistemas digitales. Proporcionan las operaciones lógicas fundamentales y las capacidades de multiplexión/demultiplexión necesarias para diseñar circuitos y sistemas digitales más complejos. El conocimiento de cómo funcionan estos componentes es crucial para comprender y diseñar circuitos y sistemas digitales.
+
+
+
+
+
